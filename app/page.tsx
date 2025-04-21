59e5245368f22007752c17d6d1ec54a4d7702915
Eric Le Donge <eric.ledonge@gmail.com>
2025-04-21 10:01:45 -0400

    ✨ Add AutoRefresh component for periodic page refresh and integrate it into the Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
+import styles from "./page.module.css";
+
 import { prisma } from "@/lib/prisma";
 
+import AutoRefresh from "./components/AutoRefresh";
 import FaunaList from "./components/FaunaList";
-import styles from "./page.module.css";
 
 export default async function Home() {
   const fauna = await prisma.fauna.findMany();
@@ -19,7 +21,10 @@ export default async function Home() {
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>Observations de Faune</h1>
+
       <FaunaList fauna={fauna} observations={observations} />
+
+      <AutoRefresh intervalMs={5000} />
     </main>
   );
 }
